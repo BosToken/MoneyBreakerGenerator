ff5e0f40d5dd4127df51b1b1516b663926178cef
Al Farisi <codelife.alf@gmail.com>
2022-02-11 21:31:40 +0700

    Revisi

--- JS/member.js
+++ JS/member.js
@@ -8,7 +8,7 @@ const member = {
         banner : "https://img5.goodfon.com/original/1440x900/7/e4/fate-grand-order-sudba-velikaia-kampaniia-assasin-devushka.jpg",
         // banner : "https://safebooru.org//samples/2672/sample_544316674c5b8e4f31d70e96536cd4ca8e40353d.jpg?2782213",
         photoProfile : "https://static.wikia.nocookie.net/fategrandorder/images/4/43/S124_Stage4.webp",
-        text : "Jadi Gini ya, ini itu web gajelas buatan salman, bukan buatanku. web gatcha sama web pemecah uang gk jelas.\n jadi bukan salah ku weeeek.",
+        text : "Jadi Gini ya, ini itu web gajelas buatanku. web gatcha sama web pemecah uang gk jelas.\n jadi ini salah ku weeeek.",
         location : "どこから？",
         company : "Gk Ada",
         highlight : "Gk Ada",
@@ -19,13 +19,13 @@ const member = {
         gitLink: "https://github.com/alfarise",
         phone: 6281515526235,
         gmail: "muhammadsalmanalfarisi82@gmail.com",
-        // banner: "https://cdn.wallpaperhub.app/cloudcache/d/3/8/7/4/1/d387416a3d0c81effa77074af64f60e0ec472426.jpg",
-        banner : "https://p4.wallpaperbetter.com/wallpaper/386/458/566/fate-series-fate-stay-night-artoria-pendragon-wallpaper-preview.jpg",
+        banner: "https://cdn.wallpaperhub.app/cloudcache/d/3/8/7/4/1/d387416a3d0c81effa77074af64f60e0ec472426.jpg",
+        // banner : "https://p4.wallpaperbetter.com/wallpaper/386/458/566/fate-series-fate-stay-night-artoria-pendragon-wallpaper-preview.jpg",
         photoProfile: "https://static.wikia.nocookie.net/fategrandorder/images/b/be/S002_Stage4.webp",
-        text : "Jadi Gini ya, ini itu web gajelas buatan salman, bukan buatanku. web gatcha sama web pemecah uang gk jelas.\n jadi bukan salah ku weeeek.",
-        location : "Rumahnya Bahrul",
-        company : "Gk Ada",
-        highlight : "Gk Ada",
+        text : "Jadi Gini ya, ini itu web gajelas buatan Faiz. web gatcha sama web pemecah uang gk jelas.",
+        location : "Unknown",
+        company : "Unknown",
+        highlight : "Unknown",
     }
 };
 
